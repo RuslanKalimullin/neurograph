87547b9c1a450c36eba01207bfae7e3ed7dcf040
Gennady Laptev <gennadylaptev@gmail.com>
2023-01-27 14:57:02 +0600

    fix config

--- neurograph/config.py
+++ neurograph/config.py
@@ -1,3 +1,6 @@
 import pathlib
 
-DATA_PATH = pathlib.Path(__file__).resolve().parent.parent.parent / 'data'
+DATA_PATH = pathlib.Path(__file__).resolve().parent.parent/ 'datasets'
+
+if not DATA_PATH.exists():
+    DATA_PATH.mkdir()
