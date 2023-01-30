a247eb44c073bc78990dcdee43424ff7d6b96af1
Gennady Laptev <gennadylaptev@gmail.com>
2023-01-30 19:06:08 +0600

    fix install pyg

--- install_pyg_via_pip.sh
+++ install_pyg_via_pip.sh
@@ -1,1 +1,1 @@
-pip install pyg-lib torch-scatter torch-sparse torch-cluster torch-spline-conv torch-geometric==2.0.3 -f https://data.pyg.org/whl/torch-1.13.0+cpu.html
+pip install pyg-lib torch-scatter torch-sparse torch-cluster torch-spline-conv torch-geometric==2.0.3 -f https://data.pyg.org/whl/torch-1.13.0.html
