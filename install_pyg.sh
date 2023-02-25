9e59b34ded1ce0b628c282a0b68fdcb09ef7365d
Gennady Laptev <gennadylaptev@gmail.com>
2023-02-25 12:03:09 +0600

    fix pytorch version

--- install_pyg.sh
+++ install_pyg.sh
@@ -1,4 +1,5 @@
 # pass `cu113` or `cpu` as the first arg
+TORCH=1.12.1
 DEVICE=$1
-pip install torch==1.12.1 --extra-index-url https://download.pytorch.org/whl/${DEVICE}
-pip install torch-geometric==2.0.3 torch-scatter torch-sparse  torch-cluster torch-spline-conv  -f https://data.pyg.org/whl/torch-1.13.0+{DEVICE}.html
+pip install torch==${TORCH} --extra-index-url https://download.pytorch.org/whl/${DEVICE}
+pip install torch-geometric==2.0.3 torch-scatter torch-sparse  torch-cluster torch-spline-conv -f https://data.pyg.org/whl/torch-${TORCH}+${DEVICE}.html
