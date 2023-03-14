3fef36d6adb42a95b34761888b99036047ae29ea
Gennady Laptev <gennadylaptev@gmail.com>
2023-03-14 13:18:39 +0600

    rm unused code

--- neurograph/utils.py
+++
@@ -1,8 +0,0 @@
-from typing import Any
-
-# we use it func throughout the project to validate args at the level
-# of specific classes, not at the level of config
-def validate_option(name: str, val: Any, options: set | list | dict):
-    msg = f'Field {name} = {val} is not supported! Available options: [{", ".join(options)}]'
-    if val not in options:
-        raise ValueError(msg)
